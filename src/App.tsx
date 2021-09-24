6bf1c20e251b1cc65a2f65a510fb804354cdf2d5
Adarsh Balachandran <Adarsh.Balachandran@hatio.in>
2021-09-24 12:06:11 +0530

    Add base routing to login and home

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
-
+import LoginBox from './components/login/login';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
-    <div>
-      <p>Turf Split</p>
-    </div>
+    <Router>
+      <Switch>
+
+        <Route exact path="/"><div>Home page</div></Route>
+        <Route path="/login">
+          <LoginBox />
+        </Route>
+
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/login/login.tsx
@@ -0,0 +1,16 @@
+import { FunctionComponent } from "react";
+import './login.css';
+interface LoginBoxProps {
+
+}
+
+const LoginBox: FunctionComponent<LoginBoxProps> = () => {
+
+    return (<>
+        <div className="login-box">
+            Login box
+        </div>
+    </>);
+}
+
+export default LoginBox;
