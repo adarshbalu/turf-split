ea6cd6207b80c0cc025148b36f6effb7c3a350a4
Adarsh Balachandran <Adarsh.Balachandran@hatio.in>
2021-09-28 11:49:55 +0530

    Add base home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import LoginBox from './components/login/login';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Home from './components/home/home';
 
 function App() {
   return (
     <Router>
       <Switch>
 
-        <Route exact path="/"><div>Home page</div></Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
         <Route path="/login">
           <LoginBox />
         </Route>
---
+++ src/components/home/home.tsx
@@ -0,0 +1,17 @@
+
+import React, { FunctionComponent } from "react";
+import './home.css';
+import '../../index.css';
+
+interface HomeProps {
+
+}
+
+const Home: FunctionComponent<HomeProps> = () => {
+    return (
+        <><h1>Home</h1>
+        </>
+    );
+}
+
+export default Home;
